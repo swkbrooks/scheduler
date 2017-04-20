3ea37ade45ac1ba6223ba9203cdd2dd107bfb670
swkbrooks <swkbrooks@gmail.com>
2017-04-20 17:07:44 -0400

    Scaffold for actual functionality

--- scheduler.py
+++ scheduler.py
@@ -2,20 +2,38 @@
 # Takes in a list of dates people CAN do
 # randomly draws, assigns them duty
 
-class Scheduler():
-
-    def __init__(self, folder_name):
-        self.foler = folder_name
-
-    class Person():
-
+class Person():
         def __init__(self, filename):
             count = 0
             file = open(filename)
             self.dateslist = []
+
+            # populating person
             for line in file:
                 if count == 0:
-                    self.name = line
+                    self.name = line.rstrip()
                 else:
-                    self.dateslist[count-1] = line
+                    self.dateslist.append(line.rstrip())
                 count += 1
+
+        def __str__(self):
+            return self.name
+
+class Scheduler():
+
+    def __init__(self, dates):
+        self.people = []
+        datefile = open (dates)
+        self.dates = []
+        for line in datesfile:
+            self.dates.append(line.rstrip())
+
+    def add(self, filename):
+        self.people.append(Person(filename))
+
+def main():
+    scheduler = Scheduler()
+    scheduler.add('staff.txt')
+    print scheduler.people[0]
+
+main()
